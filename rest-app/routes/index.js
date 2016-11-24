ee8946e82ed8a56f418e19eb19de73f9e6201dea
marcus <marcus-pf@hotmail.com>
2016-11-24 19:59:16 -0200

    Make static request

--- rest-app/routes/index.js
+++ rest-app/routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+  request('http://ec2-35-164-139-210.us-west-2.compute.amazonaws.com/hirers/111222/opportunities', function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      res.send(response); // Print the result.
+    }
+  })
 });
 
 module.exports = router;
